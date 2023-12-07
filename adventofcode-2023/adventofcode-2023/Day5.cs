21ec6ab7cc1ad0d6c4a5a3ce0205ca9a643db968
Miki <miki.pataky@gmail.com>
2023-12-07 22:43:35 +0100

    Initialization of Day 5.

---
+++ adventofcode-2023/adventofcode-2023/Day5.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AdventOfCode_2023
+{
+    static public class Day5
+    {
+        static public int AnswerPartOne()
+        {
+            return 0; 
+        }
+
+        static public int AnswerPartTwo()
+        {
+            return 0;
+        }
+    }
+}
---
+++ adventofcode-2023/adventofcode-2023Tests/Day5Tests.cs
@@ -0,0 +1,18 @@
+﻿using AdventOfCode_2023;
+
+namespace AdventOfCode_2023Tests
+{
+    public class Day5Tests
+    {
+        [Test]
+        public void PartOne()
+        {
+            Assert.That(Day5.AnswerPartOne(), Is.EqualTo(0));
+        }
+
+        public void PartTwo()
+        {
+            Assert.That(Day5.AnswerPartOne(), Is.EqualTo(0));
+        }
+    }
+}
